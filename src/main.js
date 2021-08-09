57bbe7e5e591457175e10d53e342685a0d5f5289
gtf <1179721193@qq.com>
2021-08-09 16:31:11 +0800

    项目初始化-调整初始目录结构

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
+/**
+ * 项目的启动入口
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import './styles/index.less'
+// 创建vue根实例
+// 把router配置到根实例中
+// 通过render方法把app根组件渲染到#app入口节点 13341861067
 createApp(App).use(router).mount('#app')
